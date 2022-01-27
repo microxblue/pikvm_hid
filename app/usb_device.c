08b71f5e98f64934cbd4cb658a6f73201c5d221f
Danny <danny@35g.tw>
2022-01-27 13:41:38 +0800

    Wheel is supported

--- app/usb_device.c
+++ app/usb_device.c
@@ -143,7 +143,7 @@ __ALIGN_BEGIN static uint8_t HID_KB_ReportDesc[HID_KB_REPORT_DESC_SIZE] __ALIGN_
 	0xc0,		// END_COLLECTION
 };
 
-#define HID_MS_REPORT_DESC_SIZE (52 + ABSOLUTE_MOUSE_MODE)
+#define HID_MS_REPORT_DESC_SIZE (52 + ABSOLUTE_MOUSE_MODE + 10)
 __ALIGN_BEGIN static uint8_t HID_MS_ReportDesc[HID_MS_REPORT_DESC_SIZE] __ALIGN_END = {
 	0x05, 0x01,		  // USAGE_PAGE (Generic Desktop)
 	0x09, 0x02,		  // USAGE (Mouse)
@@ -163,14 +163,13 @@ __ALIGN_BEGIN static uint8_t HID_MS_ReportDesc[HID_MS_REPORT_DESC_SIZE] __ALIGN_
 	0x81, 0x01,		  //     INPUT (Cnst)
 	0x05, 0x01,		  //     USAGE_PAGE (Generic Desktop)
 	0x09, 0x30,		  //     USAGE (X)
-	0x09, 0x31,		  //     USAGE (Y)
-	0x09, 0x38,		  //     USAGE (W)
+	0x09, 0x31,		  //     USAGE (Y) 18
 #ifdef ABSOLUTE_MOUSE_MODE
 	0x15, 0x00,		  //     LOGICAL_MINIMUM (0)
 	0x26, 0xff, 0x7f, //     LOGICAL_MAXIMUM (32767)
 	0x75, 0x10,		  //     REPORT_SIZE  (16)
-	0x95, 0x03,		  //     REPORT_COUNT (3)
-	0x81, 0x02,		  //     INPUT (Data,Var,Abs)
+	0x95, 0x02,		  //     REPORT_COUNT (2)
+	0x81, 0x02,		  //     INPUT (Data,Var,Abs) 23
 #else
 	0x15, 0x81, //     LOGICAL_MINIMUM (-127)
 	0x25, 0x7f, //     LOGICAL_MAXIMUM (127)
@@ -178,6 +177,13 @@ __ALIGN_BEGIN static uint8_t HID_MS_ReportDesc[HID_MS_REPORT_DESC_SIZE] __ALIGN_
 	0x95, 0x03, //     REPORT_COUNT (3)
 	0x81, 0x06, //     INPUT (Data,Var,Rel)
 #endif
+	//Danny Wheel Use int8_t
+	0x09, 0x38,		  //     USAGE (W)
+	0x15, 0x81,		  //     LOGICAL_MINIMUM (-127)
+	0x25, 0x7f,		  //     LOGICAL_MAXIMUM (127)
+	0x75, 0x08,		  //     REPORT_SIZE (8)
+	0x95, 0x01,		  //     REPORT_COUNT (1)
+	0x81, 0x06,		  //     INPUT (Data,Var,Rel)
 	0xc0,			  //   END_COLLECTION
 	0xc0			  // END_COLLECTION
 };
